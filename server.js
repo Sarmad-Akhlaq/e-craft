ec4efad99ff5b3efba1ca8e31e0da797646ae479
Sarmad-Akhlaq <sarmadakhlaq.66@gmail.com>
2021-06-11 20:33:02 +0500

    mongoDB atlas connected

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose")
+const mongoDB_Password = "TGPSWqfe09q99COJ";
+
+mongoose.connect('mongodb+srv://SarmadAkhlaq:TGPSWqfe09q99COJ@cluster0.xic2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then((con) => {
+    console.log("connenct to mongoDB")
+})
 
 const app = express();
 
